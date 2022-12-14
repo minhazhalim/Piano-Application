331873a0946913fd37664811e300d29e8e918e68
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2022-12-15 00:27:16 +0600

    Add files via upload

--- development.js
+++ development.js
@@ -1,9 +1,29 @@
+const white_keys = ['z','x','c','v','b','n','m'];
+const black_keys = ['s','d','g','h','j'];
 const key = document.querySelectorAll('.key');
-function playSound(newURL){
-     new Audio(newURL).play();
-}
-key.forEach((keys,index) => {
-     const number = index < 9 ? '0' + (index + 1) : (index + 1);
-     const newURL = 'musics/key' + number + '.mp3';
-     keys.addEventListener('click',() => playSound(newURL));
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+key.forEach(keys => {
+     keys.addEventListener('click',() => playNote(keys));
 });
+document.addEventListener('keydown',event => {
+     if(event.repeat) return;
+     const key = event.key;
+     const whiteKeyIndex = white_keys.indexOf(key);
+     const blackKeyIndex = black_keys.indexOf(key);
+     if(whiteKeyIndex > -1){
+          playNote(whiteKeys[whiteKeyIndex]);
+     }
+     if(blackKeyIndex > -1){
+          playNote(blackKeys[blackKeyIndex]);
+     }
+});
+function playNote(key){
+     const noteAudio = document.getElementById(key.dataset.note);
+     noteAudio.currentTime = 0;
+     noteAudio.play();
+     key.classList.add('active');
+     noteAudio.addEventListener('ended',() => {
+          key.classList.remove('active');
+     });
+}
