5669dafdcd3fc56c922cc2cd80c37fc6b949a9f4
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2022-12-15 00:26:48 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const white_keys = ['z','x','c','v','b','n','m'];
-const black_keys = ['s','d','g','h','j'];
-const key = document.querySelectorAll('.key');
-const whiteKeys = document.querySelectorAll('.key.white');
-const blackKeys = document.querySelectorAll('.key.black');
-key.forEach(keys => {
-     keys.addEventListener('click',() => playNote(keys));
+const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const volumeSlider = document.querySelector('.volume-slider input');
+const keysCheckbox = document.querySelector('.keys-checkbox input');
+let allKeys = [];
+let audio = new Audio('./tunes/a.wav');
+const playTune = (key) => {
+     audio.src = `./tunes/${key}.wav`;
+     audio.play();
+     const clickedKey = document.querySelector(`[data-key="${key}"]`);
+     clickedKey.classList.add('active');
+     setTimeout(() => {
+          clickedKey.classList.remove('active');
+     },150);
+};
+pianoKeys.forEach(key => {
+     allKeys.push(key.dataset.key);
+     key.addEventListener('click',() => playTune(key.dataset.key));
 });
-document.addEventListener('keydown',event => {
-     if(event.repeat) return;
-     const key = event.key;
-     const whiteKeyIndex = white_keys.indexOf(key);
-     const blackKeyIndex = black_keys.indexOf(key);
-     if(whiteKeyIndex > -1){
-          playNote(whiteKeys[whiteKeyIndex]);
-     }
-     if(blackKeyIndex > -1){
-          playNote(blackKeys[blackKeyIndex]);
-     }
-});
-function playNote(key){
-     const noteAudio = document.getElementById(key.dataset.note);
-     noteAudio.currentTime = 0;
-     noteAudio.play();
-     key.classList.add('active');
-     noteAudio.addEventListener('ended',() => {
-          key.classList.remove('active');
-     });
-}
+const handleVolume = (event) => {
+     audio.volume = event.target.value;
+};
+const showHideKey = () => {
+     pianoKeys.forEach(key => key.classList.toggle('hide'));
+};
+const pressedKey = (event) => {
+     if(allKeys.includes(event.key)) playTune(event.key);
+};
+keysCheckbox.addEventListener('click',showHideKey);
+volumeSlider.addEventListener('input',handleVolume);
+document.addEventListener('keydown',pressedKey);
